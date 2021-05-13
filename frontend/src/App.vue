4762d202c11fd7f33a2103aa57143a234dd552f2
alexandremr01 <alexandremr01@gmail.com>
2021-05-13 10:27:29 -0300

    add color palette

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,12 +16,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./styles/common.scss";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,6 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from './router';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
