4f47dda28b2b3f3b56a18da608a889751c505ef3
vasconcelosdavi <davi.muniz41@gmail.com>
2021-06-10 19:40:56 -0300

    get data from api

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,10 +1,22 @@
 <template>
   <h1>Dashboard</h1>
+
 </template>
+
 <script>
+import axios from 'axios';
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components : {
+
+  },
+  async created() {
+    const { data } = await axios.get("http://localhost:8080/api/stocks/?symbol=NDAQ&freq=DAY");
+    console.log(data);
+  }
 };
 </script>
+
 <style scoped>
+
 </style>
