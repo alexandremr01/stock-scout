68fd8bd098b7f09623d1d7e57904df25c1399317
vasconcelosdavi <davi.muniz41@gmail.com>
2021-06-17 22:07:09 -0300

    add opening price and closing price

---
+++ frontend/src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<script>
+import {Line} from 'vue-chartjs'
+
+export default {
+    extends: Line,
+    props: {
+        label: {
+            type: String
+        },
+        chart_data: {
+            type: Array
+        },
+        options: {
+            type: Object
+        }
+    },
+    mounted() {
+        const date = this.chart_data.map(stock_data => stock_data.date).reverse();
+        const price = this.chart_data.map(stock_data => stock_data.price).reverse();
+
+        console.log(price);
+
+        this.renderChart(
+        {
+            labels: date,
+            datasets: [{
+                label: this.label,
+                chart_data: price,
+            }]
+        },
+        this.options)
+    }
+}
+</script>
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,18 +1,51 @@
 <template>
-  <h1>Dashboard</h1>
-
+  <div id="dashboard" class="container">
+    <div class="row mt-5" v-if="opening_price.length > 0">
+      <div class="col">
+        <h2>Opening Price</h2>
+        <line-chart :chart_data="opening_price" :options="chart_options" label="Opening"></line-chart>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
+
+import LineChart from '../components/LineChart'
+
 export default {
   name: 'Dashboard',
-  components : {
-
+  components : { 
+    LineChart
+  },
+  data() {
+    return {
+      opening_price: [],
+      closing_price: [],
+      chart_options: {
+        responsive: true,
+        maintain_aspect_ratio: false
+      }
+    };
   },
   async created() {
-    const { data } = await axios.get("http://localhost:8080/api/stocks/?symbol=NDAQ&freq=DAY");
-    console.log(data);
+    var { data } = await axios.get("http://localhost:8080/api/stocks/?symbol=NDAQ&freq=DAY");
+    
+    // console.log(data);
+
+    data = JSON.parse(data);
+
+    data.forEach(data_element => {
+      const date = moment(data_element.Date, "YYYYMMDD").format("DD/MM");
+      const opening = data_element.open;
+      const closing = data_element.close;
+
+      this.opening_price.push({date, price : opening});
+      this.closing_price.push({date, price : closing});
+    });
+    console.log(this.opening_price);
   }
 };
 </script>
