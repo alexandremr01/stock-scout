c579c3a13e3c8612d7690599dffa298cda6dad6b
alexandremr01 <alexandremr01@gmail.com>
2021-07-03 12:13:11 -0300

    refactor simulation

--- frontend/src/views/Simulations.vue
+++ frontend/src/views/Simulations.vue
@@ -6,56 +6,67 @@
 
     <b-row align-h="start">
       <b-col cols="4">
-        <v-select class="style-chooser" @input="updateType" :options="calculationOptions" v-model="selectedSim" ></v-select>
+        <v-select class="style-chooser" @input="updateType" :options="calculationOptions" v-model="selectedSim"></v-select>
       </b-col>
     </b-row>
 
     <b-container>
-      <b-row class="my-1" >
-<!--        <b-form-radio v-model="selected" name="some-radios" value="A"/>-->
+      <b-row class="my-1" v-for="input in inputs" :key="input.name">
         <b-col sm="3" align="left">
-          <label>{{$t('initial')}}</label>
+          <label>{{$t(input.name)}}</label>
         </b-col>
         <b-col sm="4">
-          <b-form-input v-model="values.initial" :readonly="selected==='A'" :formatter="currencyFormat" @input="update"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="my-1" >
-<!--        <b-form-radio v-model="selected" name="some-radios" value="B"/>-->
-        <b-col sm="3" align="left">
-          <label>{{$t('monthly')}}</label>
-        </b-col>
-        <b-col sm="4">
-          <b-form-input v-model="values.monthly" :readonly="selected==='B'" :formatter="currencyFormat" @input="update"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="my-1" >
-<!--        <b-form-radio v-model="selected" name="some-radios" value="C"/>-->
-        <b-col sm="3" align="left">
-          <label>{{$t('time')}} </label>
-        </b-col>
-        <b-col sm="2">
-          <b-form-input v-model="values.time" :readonly="selected==='C'" type="number" @input="update" ></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="my-1" >
-        <b-col sm="3" align="left">
-          <label>{{$t('interest')}}</label>
-        </b-col>
-        <b-col sm="2">
-          <b-form-input v-model="values.interest" :readonly="selected==='D'" :formatter="percentageFormat" @input="update"></b-form-input>
-        </b-col>
-      </b-row>
-      <b-row class="my-1" >
-<!--        <b-form-radio v-model="selected" name="some-radios" value="E"/>-->
-        <b-col sm="3" align="left">
-          <label>{{$t('final')}}</label>
-        </b-col>
-        <b-col sm="4">
-          <b-form-input v-model="values.final" :readonly="selected==='E'" :formatter="currencyFormat" @input="update"></b-form-input>
+          <b-form-input v-model="input.value" :readonly="selected===input.name" :formatter="input.formatter" @input="update"></b-form-input>
         </b-col>
       </b-row>
     </b-container>
+
+<!--    <b-container>-->
+<!--      <b-row class="my-1" >-->
+<!--&lt;!&ndash;        <b-form-radio v-model="selected" name="some-radios" value="A"/>&ndash;&gt;-->
+<!--        <b-col sm="3" align="left">-->
+<!--          <label>{{$t('initial')}}</label>-->
+<!--        </b-col>-->
+<!--        <b-col sm="4">-->
+<!--          <b-form-input v-model="values.initial" :readonly="selected==='A'" :formatter="currencyFormat" @input="update"></b-form-input>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--      <b-row class="my-1" >-->
+<!--&lt;!&ndash;        <b-form-radio v-model="selected" name="some-radios" value="B"/>&ndash;&gt;-->
+<!--        <b-col sm="3" align="left">-->
+<!--          <label>{{$t('monthly')}}</label>-->
+<!--        </b-col>-->
+<!--        <b-col sm="4">-->
+<!--          <b-form-input v-model="values.monthly" :readonly="selected==='B'" :formatter="currencyFormat" @input="update"></b-form-input>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--      <b-row class="my-1" >-->
+<!--&lt;!&ndash;        <b-form-radio v-model="selected" name="some-radios" value="C"/>&ndash;&gt;-->
+<!--        <b-col sm="3" align="left">-->
+<!--          <label>{{$t('time')}} </label>-->
+<!--        </b-col>-->
+<!--        <b-col sm="2">-->
+<!--          <b-form-input v-model="values.time" :readonly="selected==='C'" type="number" @input="update" ></b-form-input>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--      <b-row class="my-1" >-->
+<!--        <b-col sm="3" align="left">-->
+<!--          <label>{{$t('interest')}}</label>-->
+<!--        </b-col>-->
+<!--        <b-col sm="2">-->
+<!--          <b-form-input v-model="values.interest" :readonly="selected==='D'" :formatter="percentageFormat" @input="update"></b-form-input>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--      <b-row class="my-1" >-->
+<!--&lt;!&ndash;        <b-form-radio v-model="selected" name="some-radios" value="E"/>&ndash;&gt;-->
+<!--        <b-col sm="3" align="left">-->
+<!--          <label>{{$t('final')}}</label>-->
+<!--        </b-col>-->
+<!--        <b-col sm="4">-->
+<!--          <b-form-input v-model="values.final" :readonly="selected==='E'" :formatter="currencyFormat" @input="update"></b-form-input>-->
+<!--        </b-col>-->
+<!--      </b-row>-->
+<!--    </b-container>-->
   </div>
 </template>
 
@@ -89,13 +100,13 @@ export default {
   data () {
     return {
       dialog: false,
-      selected: 'E',
+      selected: null,
       selectedSim: null,
       calculationOptions: [
-        {label: this.$t('initial'), code: 'A'},
-        {label: this.$t('monthly'), code: 'B'},
-        {label: this.$t('time'), code: 'C'},
-        {label: this.$t('final'), code: 'E'},
+        {label: this.$t('initial'), name: 'initial'},
+        {label: this.$t('monthly'), name: 'monthly'},
+        {label: this.$t('time'), name: 'time'},
+        {label: this.$t('final'), name: 'final'},
       ],
       values: {
         initial: null,
@@ -103,20 +114,30 @@ export default {
         time: null,
         interest: '6.00',
         final: null
-      }
+      },
+      inputs:[
+        {name:'initial', originalIndex: 0, formatter: this.currencyFormat, value: null},
+        {name:'monthly', originalIndex: 1, formatter: this.currencyFormat, value: null},
+        {name:'time', originalIndex: 2, value: null},
+        {name:'interest', originalIndex: 3, formatter: this.percentageFormat, value: '6.0'},
+        {name:'final', originalIndex: 4, formatter: this.currencyFormat, value: null},
+      ]
     }
   },
   mounted(){
-    this.selectedSim = this.calculationOptions[0];
+    this.selectedSim = this.calculationOptions[3];
+    this.selected = 'final';
   },
   methods: {
     currencyFormat(value){
       return this.toCurrency(this.toNumber(value)/100)
     },
     percentageFormat(value){
-      return value.replace(/[^0-9.-]+/g,"");
+      return value.replace(/[^0-9.]+/g,"");
     },
     update(){
+      this.inputs.forEach((v, i,a) => this.values[v.name] = v.value)
+
       let initial = (this.selected !== 'A') ? this.fromText(this.values.initial) : null;
       let monthly = (this.selected !== 'B') ? this.fromText(this.values.monthly) : null;
       let interest = (this.selected !== 'D') ? this.fromText(this.values.interest)/100 : null;
@@ -125,22 +146,22 @@ export default {
       let monthlyInterest = Math.pow(1+interest, 1/12);
       let ratio = monthlyInterest**(this.values.time);
 
-      if (this.selected === 'E') {
+      if (this.selected === 'final') {
         if (this.values.initial == null || this.values.interest==null || this.values.monthly == null || this.values.time == null)
           return;
         let final = initial * ratio + monthly*(ratio-1)/(monthlyInterest-1);
         this.values.final = this.toCurrency(final);
-      } else if (this.selected === 'A') {
+      } else if (this.selected === 'initial') {
         if (this.values.final == null || this.values.interest==null || this.values.monthly == null || this.values.time == null)
           return;
         let init = (finalValue - monthly*(ratio-1)/(monthlyInterest-1))/ratio;
         this.values.initial = this.toCurrency(init);
-      } else if (this.selected === 'B') {
+      } else if (this.selected === 'monthly') {
         if (this.values.final == null || this.values.interest==null || this.values.initial == null || this.values.time == null)
           return;
         let monthly = ((finalValue - initial*ratio) * (monthlyInterest-1))/(ratio-1);
         this.values.monthly = this.toCurrency(monthly);
-      } else if (this.selected === 'C') {
+      } else if (this.selected === 'time') {
         if (this.values.final == null || this.values.interest==null || this.values.initial == null || this.values.monthly == null)
           return;
         let aux = monthly/(monthlyInterest-1);
@@ -148,9 +169,11 @@ export default {
         let time = Math.log(x) / Math.log(monthlyInterest);
         this.values.time = Math.ceil(time);
       }
+      this.inputs.forEach((v, i,a) => v.value=this.values[v.name])
+
     },
     toNumber(n){
-      return Number(n.replace(/[^0-9-]+/g,""));
+      return Number(n.replace(/[^0-9]+/g,""));
     },
     toCurrency(n){
       let currency = i18n.locale === 'pt-br'? 'BRL' : 'USD';
@@ -161,7 +184,19 @@ export default {
       return this.toNumber(n)/100;
     },
     updateType(v){
-      this.selected = v.code;
+      console.log(this.inputs)
+      this.selected = v.name;
+      let retIndex = this.inputs[4].originalIndex;
+      let bkp = this.inputs[retIndex];
+      this.inputs[retIndex] = this.inputs[4];
+      this.inputs[4] = bkp;
+
+      let res = this.inputs.find(x => x.name === v.name);
+      this.inputs[res.originalIndex] = this.inputs[4];
+      this.inputs[4] = res;
+      // let res = this.inputs.findIndex(x => x.code === v.code);
+      // let cutOut = this.inputs.splice(res, 1) [0]; // cut the element at index 'from'
+      // this.inputs.splice(5, 0, cutOut);            // insert it at index 'to'
     }
   },
 };
