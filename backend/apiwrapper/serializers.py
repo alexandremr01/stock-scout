078afdc71597bdce9472d0d15533815edcf7decf
kenji-yamane <kenjiyamane0@gmail.com>
2021-07-03 20:21:33 -0300

    add daily history aux function

--- backend/apiwrapper/serializers.py
+++ backend/apiwrapper/serializers.py
@@ -1,7 +1,8 @@
 import json
 
 from rest_framework import serializers
-from .models import CoinQuotation
+from .models import StockTimeSeries, CoinQuotation
+from .api_client import alpha_vantage_client
 
 class CoinQuotationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,3 +30,23 @@ def alphav_to_ss(alphav_data, freq):
         ss_arr.append(ss_dict)
     
     return json.dumps(ss_arr)
+
+# json to map
+def get_daily_history(symbol):
+    time_series = None
+    query = StockTimeSeries.objects.filter(ticker=symbol).filter(frequency='DAY')
+
+    if query.exists():
+        time_series = query[0]
+    else:
+        time_series = query[0]
+        alphav_json = alpha_vantage_client(symbol, 'DAY')
+        ss_json = alphav_to_ss(alphav_json, 'DAY')
+        new_time_series = StockTimeSeries(ticker=symbol, data=ss_json, frequency='DAY')
+        new_time_series.save()
+        time_series = new_time_series
+
+    daily_hist = {}
+    for daily_intel in eval(time_series.data):
+        daily_hist[daily_intel['Date']] = daily_intel['close']
+    return daily_hist
--- backend/apiwrapper/views.py
+++ backend/apiwrapper/views.py
@@ -7,7 +7,7 @@ from rest_framework import permissions
 from datetime import datetime, timedelta, timezone
 
 from .api_client import alpha_vantage_client, hg_brasil_client
-from .serializers import alphav_to_ss, CoinQuotationSerializer
+from .serializers import alphav_to_ss, CoinQuotationSerializer, get_daily_history
 
 # Create your views here.
 class StockDetail(APIView):
@@ -20,21 +20,20 @@ class StockDetail(APIView):
     def get(self, request, format=None):
         symbol = request.query_params.get('symbol')
         freq = request.query_params.get('freq')
-        filter1 = StockTimeSeries.objects.filter(ticker=symbol)
-        if filter1.exists():
-            filter2 = filter1.filter(frequency=freq)
-            if filter2.exists():
-                time_series = filter2[0]
-                refresh = {'DAY': 1, 'WEEK': 7, 'MONTH': 30}
+        h = get_daily_history(symbol)
+        query = StockTimeSeries.objects.filter(ticker=symbol).filter(frequency=freq)
+        if query.exists():
+            time_series = query[0]
+            refresh = {'DAY': 1, 'WEEK': 7, 'MONTH': 30}
 
-                if datetime.now(timezone.utc) - time_series.last_modified < timedelta(days=refresh[freq]):
-                    return Response(time_series.data)
+            if datetime.now(timezone.utc) - time_series.last_modified < timedelta(days=refresh[freq]):
+                return Response(time_series.data)
 
-                alphav_json = alpha_vantage_client(symbol, freq)
-                ss_json = alphav_to_ss(alphav_json, freq)
-                time_series.data = ss_json
-                time_series.save()
-                return Response(ss_json)
+            alphav_json = alpha_vantage_client(symbol, freq)
+            ss_json = alphav_to_ss(alphav_json, freq)
+            time_series.data = ss_json
+            time_series.save()
+            return Response(ss_json)
 
         alphav_json = alpha_vantage_client(symbol, freq)
         ss_json = alphav_to_ss(alphav_json, freq)
