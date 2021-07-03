39151221a8eb75c20d3ebb9d65ef124109576c70
alexandremr01 <alexandremr01@gmail.com>
2021-07-03 19:46:36 -0300

    add operations

--- backend/wallets/views.py
+++ backend/wallets/views.py
@@ -5,13 +5,55 @@ from rest_framework import authentication, permissions
 from rest_framework.views import APIView
 from django.http import JsonResponse
 
-from .serializers import WalletSerializer
-from .models import Wallet
+from .serializers import WalletSerializer, OperationSerializer
+from .models import Wallet, Operations
+from users.models import Profile
+from rest_framework.response import Response
+import json
 
 
 class WalletViewSet(viewsets.ModelViewSet):
     queryset = Wallet.objects.all().order_by('name')
     serializer_class = WalletSerializer
+
+    def list(self, request):
+        user = request.user
+        profile = Profile.objects.filter(user = user).first()
+        queryset = Wallet.objects.filter(profile = profile)
+        serializer = WalletSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, pk=None):
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        user = request.user
+        profile = Profile.objects.filter(user = user).first()
+        wallet = Wallet(profile=profile, name=body['name'])
+        wallet.save()
+        return Response(WalletSerializer(wallet).data)
+
+
+class OperationsViewSet(viewsets.ModelViewSet):
+    queryset = Operations.objects.all().order_by('day')
+    serializer_class = OperationSerializer
+
+    def list(self, request, pk):
+        wallet = Wallet.objects.filter(id=pk).first()
+        print(wallet.name)
+        print("hers")
+        queryset = Operations.objects.filter(wallet=wallet)
+        serializer = OperationSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def create(self, request, pk):
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        wallet = Wallet.objects.filter(id=pk).first()
+        sim = Operations(wallet=wallet, day=body['day'], type=body['type'],
+                         quantity=body['quantity'], value=body['value'], symbol=body['symbol'])
+        sim.save()
+        return Response(OperationSerializer(sim).data)
 
 
 class PhraseView(APIView):
