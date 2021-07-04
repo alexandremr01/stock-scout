91d8efc4a1b6273892432410eacd1c74a8738db6
alexandremr01 <alexandremr01@gmail.com>
2021-07-04 14:49:38 -0300

    consolidated wallet endpoint

--- backend/wallets/views.py
+++ backend/wallets/views.py
@@ -4,6 +4,7 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework import authentication, permissions
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from django.db import connection
 
 from .serializers import WalletSerializer, OperationSerializer
 from .models import Wallet, Operations
@@ -22,6 +23,32 @@ class WalletViewSet(viewsets.ModelViewSet):
         queryset = Wallet.objects.filter(profile = profile)
         serializer = WalletSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def retrieve(self, request, pk):
+        user = request.user
+        profile = Profile.objects.filter(user=user).first()
+        queryset = Wallet.objects.filter(profile=profile)
+        rows = []
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT symbol, 
+                   SUM(CASE WHEN type = 'buy' THEN quantity ELSE -quantity END) quantity,
+                   SUM(CASE WHEN type = 'buy' THEN value END) paid_value,
+                   SUM(CASE WHEN type = 'buy' THEN quantity END) total_bought
+            FROM wallets_operations 
+            WHERE wallet_id = %s 
+            GROUP BY symbol""", [pk])
+            rows = cursor.fetchall()
+        serializer = WalletSerializer(queryset, many=True)
+        resp = []
+        print(rows)
+        for r in rows:
+            if r[3] == 0 or r[3] is None:
+                avg = 0
+            else:
+                avg = r[2]/r[3]
+            resp.append({'symbol': r[0], 'quantity': r[1], 'avg_value': avg})
+        return JsonResponse({'stocks': resp})
 
     def create(self, request, pk=None):
         body_unicode = request.body.decode('utf-8')
