4dde03d9ef8b95819d9fc11248f061c2c0c907ac
vasconcelosdavi <davi.muniz41@gmail.com>
2021-07-05 19:39:54 -0300

    remove phone number from signup

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -53,8 +53,9 @@
           >
             <b-form-input
               id="input-4"
-              v-model="form.phoneNumber"
-              placeholder="Enter phone number"
+              v-model="form.password"
+              placeholder="Enter password"
+              type="password"
               required
             ></b-form-input>
           </b-form-group>
@@ -66,20 +67,6 @@
           >
             <b-form-input
               id="input-5"
-              v-model="form.password"
-              placeholder="Enter password"
-              type="password"
-              required
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group
-            class="align-label"
-            id="input-group-6"
-            label-for="input-6"
-          >
-            <b-form-input
-              id="input-6"
               v-model="form.confirmPassword"
               placeholder="Confirm password"
               type="password"
