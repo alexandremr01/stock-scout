d7f5651cae7fca261a9b9c88d101492d0bdbeec8
kenji-yamane <kenjiyamane0@gmail.com>
2021-07-05 00:22:29 -0300

    refactor wallets models

--- backend/wallets/admin.py
+++ backend/wallets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Wallet, Asset
+from .models import Wallet, Operations
 
 # Register your models here.
 admin.site.register(Wallet)
-admin.site.register(Asset)
+admin.site.register(Operations)
