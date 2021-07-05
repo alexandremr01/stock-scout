f4e02d80be4a7a83a6593cac22241e165c3b6b38
Alvaro Tedeschi <alvaro_porc@hotmail.com>
2021-07-05 18:27:16 -0300

    fixed dashboard layout bug

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -368,6 +368,10 @@ export default {
 .extremecontainer {
   width: 100%;
   height: 20%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
 
 .topcontainer {
